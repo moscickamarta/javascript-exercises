fd8d3f940fd5864bd96d878f2165a9b470a30b4e
moscickamarta <249362937+moscickamarta@users.noreply.github.com>
2025-12-28 11:47:13 +0100

    finished exercise

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
+const reverseString = function(word) {
+let solution='';
 
+for(let i=word.length-1; i>=0; i--){
+  solution += word[i];
+}
+return solution;
 };
 
 // Do not edit below this line

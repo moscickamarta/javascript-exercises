4fcc23cf03ca5adb021d9f3d0b73d22713714347
moscickamarta <249362937+moscickamarta@users.noreply.github.com>
2025-12-28 11:36:59 +0100

    Finished exercise

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
+const repeatString = function(string, times) {
 
+  let solution='';
+
+  if(times<0)
+    return 'ERROR';
+
+  else while(times>0){
+    solution += string;
+    times--;
+  }
+  return solution;
 };
 
 // Do not edit below this line

b3f2af7a9ca1fc2a5fd07ddd11619c68847d851c
moscickamarta <249362937+moscickamarta@users.noreply.github.com>
2026-01-05 18:02:50 +0100

    finished exercise

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
-const removeFromArray = function() {
-};
+const removeFromArray = function(array) {
+
+  for(let ind=array.length-1; ind>=0; ind--) {
+    for(let i=arguments.length-1; i>=0; i--){
+       if (array[ind] === arguments[i]){
+        array.splice(ind,1)}
+    }
+    }
+  
+  return array;
+  };
 
 // Do not edit below this line
 module.exports = removeFromArray;
+

f7faeac45f13ca63911a845666cb730c3dcccdd2
moscickamarta <249362937+moscickamarta@users.noreply.github.com>
2026-01-05 18:40:56 +0100

    finished exercise

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,26 @@
-const sumAll = function() {
+const sumAll = function(first, second) {
 
+if(first<0 || second<0){
+return 'ERROR';
+}
+
+if(Number.isInteger(first)===false || Number.isInteger(second)===false){
+return 'ERROR';
+}
+
+let buffor=0;
+if(second<first){
+  buffor=first;
+  first=second;
+  second=buffor;
+}
+let value=0;
+
+for(let i=first; i<=second; i++){
+  value+=i;
+}
+
+return value;
 };
 
 // Do not edit below this line
